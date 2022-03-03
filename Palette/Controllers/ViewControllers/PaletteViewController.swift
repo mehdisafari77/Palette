6f216cdc5757499caf1de908ae38b46c218c7f49
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-02 16:41:39 -0800

    add initial stack view

---
+++ Palette/Controllers/ViewControllers/PaletteViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PaletteViewController.swift
+//  Palette
+//
+//  Created by Mehdi MMS on 02/03/2022.
+//  Copyright © 2022 Cameron Stuart. All rights reserved.
+//
+
+import UIKit
+
+class PaletteViewController: UIViewController {
+    
+    //MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Views
+    let featureButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Feature", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.contentHorizontalAlignment = .center
+        
+        return button
+    }()
+    
+    let randomButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Random", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.contentHorizontalAlignment = .center
+
+        return button
+    }()
+    
+    let doubleRainbowButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Double Rainbow", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.contentHorizontalAlignment = .center
+
+        return button
+    }()
+    
+    let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalCentering
+        
+        
+        return stackView
+    }()
+
+} // End of Class
